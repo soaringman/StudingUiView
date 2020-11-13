00c2146f4d2552e8dd3cc6e95d18edf68e3cf750
Alexey Gulyaev <ghost-shadows@yandex.ru>
2020-11-13 15:55:11 +0300

    added iboutlet and ibaction in code

--- StudingUiKit/StudingUiKit/ViewController.swift
+++ StudingUiKit/StudingUiKit/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+//создадим Outlet (ссылку по которой в коде можно будет менять свойства label) в коде на label путем протягивания связи с label в тело класса UiViewController
+    @IBOutlet weak var label: UILabel!
 
+    //То же самое самое сделаем для кнопки (Button) с той лишь разницей что у нее есть еще и IBAction (задание действий кнопки, по аналогии можно так же обращаться к ее свойствам)
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+//IBAction создаем снизу как метод, с типом Uibutton
+    @IBAction func pressedButton(_ sender: UIButton) {
+        label.text = "Hello, world"
+    }
 }
 
